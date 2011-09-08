8504a2c83a255f48d4c1f3d8123686f1151cfb06
Tim Dettrick <t.dettrick@uq.edu.au>
2011-09-08 10:38:20 +1000

    Adding initial templates.

---
+++ spec/jquery-w3cdtf.spec.js
@@ -0,0 +1,7 @@
+describe('jquery-w3cdtf', function() {
+
+	it('is a jquery plugin', function() {
+		expect(typeof($.fn.w3cdtf)).toBe('function');
+	});
+
+});
---
+++ src/jquery-w3cdtf.js
@@ -0,0 +1,7 @@
+(function( $ ){
+  $.fn.w3cdtf = function() {
+
+    // Do your awesome plugin stuff here
+
+  };
+})( jQuery );
